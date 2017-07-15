70765d4a809256794c9692ea9fbf018745a5054c
Marcos Sombra <markos.vs@live.com>
2017-07-15 08:35:59 -0300

    criando página inicial

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="keywords" content="Creme Dental, Alho, Higiene Bucal, Dente">
+		<meta name="author" content="Marcos Vinicius Sombra">
+		<title>Creme Dental Sabor Alho</title>
+	</head>
+
+	<body>
+	</body>
+</html>
