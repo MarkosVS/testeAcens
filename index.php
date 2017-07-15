7973909def8b0197322b0cf7bc5ceb9eea669ee9
Marcos Sombra <markos.vs@live.com>
2017-07-15 10:52:14 -0300

    att fonte menu

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 		<link href="css/style.css" rel="stylesheet" type="text/css">
 		<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 		<link href="icon.png" rel="icon">
 
 		<title>Garlic Toothpaste</title>
