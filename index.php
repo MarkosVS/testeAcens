268a7d00da185d8b24999dfeea122cbeb5056199
Marcos Sombra <markos.vs@live.com>
2017-07-15 08:38:11 -0300

    linkando css

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 		<meta charset="utf-8">
 		<meta name="keywords" content="Creme Dental, Alho, Higiene Bucal, Dente">
 		<meta name="author" content="Marcos Vinicius Sombra">
+
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		
 		<title>Creme Dental Sabor Alho</title>
 	</head>
 
