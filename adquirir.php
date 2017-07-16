f1fefd629f2473e0f1c6457b9585af28f59fe750
Marcos Sombra <markos.vs@live.com>
2017-07-16 19:09:27 -0300

    add pagina para adquirir

---
+++ adquirir.php
@@ -0,0 +1,21 @@
+<?PHP 
+    include("includes/head.php");
+?>
+
+		<title>Garlic Toothpaste - Adquirir</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>Garlic Toothpaste</h1>
+		</header>
+
+		<?PHP
+            include("includes/menu.php");
+        ?>
+        
+        <section id="corpo">
+            
+        </section>
+	</body>
+</html>
--- includes/menu.php
+++ includes/menu.php
@@ -1,6 +1,6 @@
 <nav>
 			<ul id="menu">
 				<li><a href="index.php">Home</a></li>
-				<li><a href="#">Adquirir</a></li>
+				<li><a href="adquirir.php">Adquirir</a></li>
 			</ul>
 		</nav>
